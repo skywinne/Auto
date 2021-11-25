400b3eaf5ff3edd42cc3bad2c17f1b215468e8ae
丁水长 <dsc1697@163.com>
2021-11-25 22:18:04 +0800

    提交测试

--- python_tools/prompt_toolkit_test.py
+++ python_tools/prompt_toolkit_test.py
@@ -3,7 +3,7 @@ from prompt_toolkit import prompt
 from prompt_toolkit.history import FileHistory
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 # from prompt_toolkit.contrib.completers import WordCompleter
-
+# test
 # SQLCompleter = WordCompleter(['select', 'from', 'insert', 'update', 'delete'], ignore_case=True)
 
 while True:
